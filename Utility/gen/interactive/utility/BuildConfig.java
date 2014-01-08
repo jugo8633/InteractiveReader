1057717b4e5b3b21818e2cc263d2b9e7fc844b6e
jugo8633 <jugo.tw@gmail.com>
2014-01-08 17:07:23 +0800

    add

---
+++ Utility/gen/interactive/utility/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package interactive.utility;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Utility/src/interactive/utility/MainActivity.java
@@ -0,0 +1,15 @@
+package interactive.utility;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+
+}
