15f9d11101b89033d7769f238b81e6914f7e487d
jugo8633 <jugo.tw@gmail.com>
2014-01-09 18:13:17 +0800

    update

--- Reader/src/interactive/reader/ReaderActivity.java
+++ Reader/src/interactive/reader/ReaderActivity.java
@@ -1,8 +1,8 @@
 package interactive.reader;
 
+import interactive.common.Logs;
 import android.app.Activity;
 import android.os.Bundle;
-
 
 public class ReaderActivity extends Activity
 {
@@ -12,5 +12,15 @@ public class ReaderActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 
+		int nResId = getResourceId("reader", "layout");
+
+		this.setContentView(nResId);
+
+		Logs.showTrace("Reader Activity Create");
+	}
+
+	public int getResourceId(String name, String defType)
+	{
+		return getResources().getIdentifier(name, defType, getPackageName());
 	}
 }
