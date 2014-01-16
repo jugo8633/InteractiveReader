fc5e86b9a44083c329f546107cb4135e1a87650d
jugo8633 <jugo.tw@gmail.com>
2014-01-16 17:13:36 +0800

    add fingerpaint

---
+++ InteractiveView/src/interactive/view/postcard/FingerPaint.java
@@ -0,0 +1,25 @@
+package interactive.view.postcard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FingerPaint extends View
+{
+
+	public FingerPaint(Context context)
+	{
+		super(context);
+	}
+
+	public FingerPaint(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	public FingerPaint(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
+
+}
