f9b9fb9eaf3bb7640c200de70fe097168bbcc879
jugo8633 <jugo.tw@gmail.com>
2014-01-17 17:46:41 +0800

    add InteractiveView/src/interactive/view/animation/

---
+++ InteractiveView/src/interactive/view/animation/flipcard/Rotate3d.java
@@ -0,0 +1,132 @@
+package interactive.view.animation.flipcard;
+
+import interactive.common.Type;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+
+public class Rotate3d
+{
+
+	public static final int						ROTATE_LEFT				= 0;
+	public static final int						ROTATE_RIGHT			= 1;
+	private int									mnDirection				= Type.INVALID;
+	private SparseArray<OnRotateEndListener>	listOnRotateEndListener	= null;
+
+	public interface OnRotateEndListener
+	{
+		public void onRotateEnd();
+	}
+
+	public Rotate3d()
+	{
+		super();
+		listOnRotateEndListener = new SparseArray<OnRotateEndListener>();
+	}
+
+	public void setlistOnRotateEndListener(Rotate3d.OnRotateEndListener listener)
+	{
+		if (null != listOnRotateEndListener && null != listener)
+		{
+			listOnRotateEndListener.put(listOnRotateEndListener.size(), listener);
+		}
+	}
+
+	private void notifyRotateEnd()
+	{
+		if (null != listOnRotateEndListener)
+		{
+			for (int i = 0; i < listOnRotateEndListener.size(); ++i)
+			{
+				listOnRotateEndListener.get(i).onRotateEnd();
+			}
+		}
+	}
+
+	public void applyRotation(View view, float start, float end, int nDirection)
+	{
+		mnDirection = nDirection;
+
+		// 計算中心點
+		final float centerX = view.getWidth() / 2.0f;
+		final float centerY = view.getHeight() / 2.0f;
+
+		Rotate3dAnimation rotation = new Rotate3dAnimation(start, end, centerX, centerY, 310.0f, true);
+		rotation.setDuration(500);
+		rotation.setFillAfter(true);
+		rotation.setInterpolator(new AccelerateInterpolator());
+		// 設置監聽
+		rotation.setAnimationListener(new DisplayNextView(view));
+
+		view.startAnimation(rotation);
+		rotation = null;
+	}
+
+	private final class DisplayNextView implements Animation.AnimationListener
+	{
+		private View	mView	= null;
+
+		public DisplayNextView(View view)
+		{
+			mView = view;
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation)
+		{
+			notifyRotateEnd();
+			mView.post(new SwapViews(mView));
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation)
+		{
+
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation)
+		{
+
+		}
+	}
+
+	private final class SwapViews implements Runnable
+	{
+		private View	mView	= null;
+
+		public SwapViews(View view)
+		{
+			mView = view;
+		}
+
+		@Override
+		public void run()
+		{
+			final float centerX = mView.getWidth() / 2.0f;
+			final float centerY = mView.getHeight() / 2.0f;
+			Rotate3dAnimation rotation = null;
+
+			mView.requestFocus();
+
+			switch (mnDirection)
+			{
+			case ROTATE_LEFT:
+				rotation = new Rotate3dAnimation(90, 0, centerX, centerY, 310.0f, false);
+				break;
+			case ROTATE_RIGHT:
+				rotation = new Rotate3dAnimation(-90, 0, centerX, centerY, 310.0f, false);
+				break;
+			}
+
+			rotation.setDuration(500);
+			rotation.setFillAfter(true);
+			rotation.setInterpolator(new DecelerateInterpolator());
+			// 開始動畫
+			mView.startAnimation(rotation);
+			rotation = null;
+		}
+	}
+}
---
+++ InteractiveView/src/interactive/view/animation/flipcard/Rotate3dAnimation.java
@@ -0,0 +1,69 @@
+package interactive.view.animation.flipcard;
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Transformation;
+
+public class Rotate3dAnimation extends Animation
+{
+	private final float		mFromDegrees;
+	private final float		mToDegrees;
+	private final float		mCenterX;
+	private final float		mCenterY;
+	private final float		mDepthZ;
+	private final boolean	mReverse;		// 是否需要扭曲
+	private Camera			mCamera;		// Camera類是用來實現繞Y軸旋轉後透視投影
+
+	public Rotate3dAnimation(float fromDegrees, float toDegrees, float centerX, float centerY, float depthZ,
+			boolean reverse)
+	{
+		mFromDegrees = fromDegrees;
+		mToDegrees = toDegrees;
+		mCenterX = centerX;
+		mCenterY = centerY;
+		mDepthZ = depthZ;
+		mReverse = reverse;
+	}
+
+	@Override
+	public void initialize(int width, int height, int parentWidth, int parentHeight)
+	{
+		super.initialize(width, height, parentWidth, parentHeight);
+		mCamera = new Camera();
+	}
+
+	@Override
+	protected void applyTransformation(float interpolatedTime, Transformation t)
+	{
+		final float fromDegrees = mFromDegrees;
+		// 生成中間角度
+		float degrees = fromDegrees + ((mToDegrees - fromDegrees) * interpolatedTime); // 旋轉角度（angle）
+
+		final float centerX = mCenterX;
+		final float centerY = mCenterY;
+		final Camera camera = mCamera;
+
+		// 通過t.getMatrix()取得當前的矩陣，然後通過camera.translate來對矩陣進行平移變換
+		final Matrix matrix = t.getMatrix();
+		camera.save();
+		if (mReverse)
+		{
+			camera.translate(0.0f, 0.0f, mDepthZ * interpolatedTime);
+		}
+		else
+		{
+			camera.translate(0.0f, 0.0f, mDepthZ * (1.0f - interpolatedTime));
+		}
+		camera.rotateY(degrees); // camera.rotateY進行旋轉
+		camera.getMatrix(matrix); // 取得變換後的矩陣
+		camera.restore();
+
+		matrix.preTranslate(-centerX, -centerY);
+		matrix.postTranslate(centerX, centerY);
+	}
+
+}
