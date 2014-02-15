5e842cffa4a3730fc7d8849291fe0a72ce2961a8
jugo8633 <jugo.tw@gmail.com>
2014-02-15 15:55:36 +0800

    update zoomhandler

--- InteractiveView/src/interactive/view/animation/zoom/ZoomHandler.java
+++ InteractiveView/src/interactive/view/animation/zoom/ZoomHandler.java
@@ -1,17 +1,5 @@
 package interactive.view.animation.zoom;
 
-<<<<<<< HEAD
-import interactive.common.Device;
-import interactive.view.slideshow.SlideshowView;
-
-import android.animation.Animator;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.view.View;
-import android.view.View.MeasureSpec;
-import android.widget.ImageView;
-=======
 import interactive.common.EventHandler;
 import interactive.common.EventMessage;
 import android.animation.Animator;
@@ -26,72 +14,10 @@ import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
->>>>>>> 204daa84b89a9ac70a47661bbbb93e9f8e9deb15
 
 public class ZoomHandler
 {
 
-<<<<<<< HEAD
-	private Animator	mCurrentAnimator;
-
-	public ZoomHandler()
-	{
-		super();
-	}
-
-	public void zoomOut(Context context, View view)
-	{
-		if (mCurrentAnimator != null)
-		{
-			mCurrentAnimator.cancel();
-		}
-
-		// Load the high-resolution "zoomed-in" image.
-		final ImageView expandedImageView = new ImageView(context);
-		Bitmap bitmap = loadBitmapFromView(context, view);
-		expandedImageView.setImageBitmap(bitmap);
-	}
-
-	private Bitmap loadBitmapFromView(Context context, View v)
-	{
-		int nWidth = v.getLayoutParams().width;
-		int nHeight = v.getLayoutParams().height;
-
-		if (0 >= nWidth)
-		{
-			nWidth = getDisplayWidth(context);
-		}
-
-		if (0 >= nHeight)
-		{
-			nHeight = getDisplayHeight(context);
-		}
-
-		Bitmap b = Bitmap.createBitmap(nWidth, nHeight, Bitmap.Config.ARGB_8888);
-		Canvas c = new Canvas(b);
-		v.measure(MeasureSpec.makeMeasureSpec(v.getLayoutParams().width, MeasureSpec.EXACTLY),
-				MeasureSpec.makeMeasureSpec(v.getLayoutParams().height, MeasureSpec.EXACTLY));
-		v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
-		v.draw(c);
-
-		return b;
-	}
-
-	private int getDisplayWidth(Context context)
-	{
-		Device device = new Device(context);
-		int nWidth = device.getDeviceWidth();
-		device = null;
-		return nWidth;
-	}
-
-	private int getDisplayHeight(Context context)
-	{
-		Device device = new Device(context);
-		int nHeight = device.getDeviceHeight();
-		device = null;
-		return nHeight;
-=======
 	private Handler		notifyHandler		= null;
 	private Animator	mCurrentAnimator	= null;
 	/**
@@ -322,6 +248,5 @@ public class ZoomHandler
 	public void setNotifyHandler(Handler handler)
 	{
 		notifyHandler = handler;
->>>>>>> 204daa84b89a9ac70a47661bbbb93e9f8e9deb15
 	}
 }
