70cb1c64d6d6941e7bf4e9604ec4e4f90d42b53c
jugo8633 <jugo.tw@gmail.com>
2014-03-11 18:15:44 +0800

    fix

--- BookshelfUser/src/interactive/bookshelfuser/BookshelfUserActivity.java
+++ BookshelfUser/src/interactive/bookshelfuser/BookshelfUserActivity.java
@@ -2,13 +2,19 @@ package interactive.bookshelfuser;
 
 import interactive.common.Device;
 import interactive.common.Logs;
+import interactive.common.Type;
 import interactive.view.flip.AnimationType;
 import interactive.view.global.Global;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 import android.app.Activity;
 
@@ -18,8 +24,10 @@ public class BookshelfUserActivity extends Activity
 	private FootbarHandler	footbar					= null;
 	private ViewFlipper		flipper					= null;
 	private int				mnCurrentFootbarItem	= 0;
-	private AnimationType	animationType			= null;
-	Animation				slide_in_left, slide_out_right, slide_in_right, slide_out_left;
+	private WelcomePage		welcomePage				= null;
+	private int				mnListMenuBtnId			= Type.INVALID;
+	private DrawerLayout	drawerLayout			= null;
+	private ImageView		listMenuBtn				= null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -32,13 +40,20 @@ public class BookshelfUserActivity extends Activity
 		Global.theActivity = this;
 		Global.handlerActivity = selfHandler;
 
+		/** show welcome page */
+		welcomePage = new WelcomePage(this);
+		welcomePage.show();
+
 		/** load book layout */
 		int nResId = Global.getResourceId(this, "activity_main", "layout");
 		this.setContentView(nResId);
+
+		/** init flip */
 		flipper = (ViewFlipper) this.findViewById(Global.getResourceId(this, "viewflipper", "id"));
-		animationType = new AnimationType();
+		AnimationType animationType = new AnimationType();
 		flipper.setOutAnimation(animationType.outToLeftAnimation(200));
 		flipper.setInAnimation(animationType.inFromRightAnimation(500));
+		animationType = null;
 
 		/** init footbar handler */
 		footbar = new FootbarHandler(this);
@@ -52,6 +67,53 @@ public class BookshelfUserActivity extends Activity
 				Logs.showTrace("Footbar item selected index=" + nIndexSelected);
 			}
 		});
+
+		/** init list menu button */
+		mnListMenuBtnId = Global.getResourceId(this, "listMenuBtn", "id");
+		listMenuBtn = (ImageView) this.findViewById(mnListMenuBtnId);
+		listMenuBtn.setOnClickListener(buttonClick);
+
+		/** init drawer layout */
+		drawerLayout = (DrawerLayout) this.findViewById(Global.getResourceId(this, "drawer_layout", "id"));
+		drawerLayout.setDrawerListener(new DrawerListener()
+		{
+			@Override
+			public void onDrawerClosed(View arg0)
+			{
+				drawerHandler(false);
+			}
+
+			@Override
+			public void onDrawerOpened(View arg0)
+			{
+				drawerHandler(true);
+			}
+
+			@Override
+			public void onDrawerSlide(View arg0, float arg1)
+			{
+
+			}
+
+			@Override
+			public void onDrawerStateChanged(int arg0)
+			{
+
+			}
+		});
+
+	}
+
+	private void drawerHandler(boolean bOpen)
+	{
+		if (bOpen)
+		{
+			listMenuBtn.setImageResource(Global.getResourceId(this, "list_click", "drawable"));
+		}
+		else
+		{
+			listMenuBtn.setImageResource(Global.getResourceId(this, "list_normal", "drawable"));
+		}
 	}
 
 	private void getDeviceInfo(Activity activity)
@@ -80,14 +142,38 @@ public class BookshelfUserActivity extends Activity
 		Logs.showTrace("Flipper show child index=" + nIndex);
 	}
 
-	private Handler	selfHandler	= new Handler()
-								{
-									@Override
-									public void handleMessage(Message msg)
-									{
-										super.handleMessage(msg);
-									}
+	private void handleButtonClick(View view)
+	{
+		if (view.getId() == mnListMenuBtnId)
+		{
+			if (drawerLayout.isDrawerOpen(Gravity.LEFT))
+			{
+				drawerLayout.closeDrawers();
+			}
+			else
+			{
+				drawerLayout.openDrawer(Gravity.LEFT);
+			}
+		}
+	}
 
-								};
+	private OnClickListener	buttonClick	= new OnClickListener()
+										{
+											@Override
+											public void onClick(View view)
+											{
+												handleButtonClick(view);
+											}
+										};
+
+	private Handler			selfHandler	= new Handler()
+										{
+											@Override
+											public void handleMessage(Message msg)
+											{
+												super.handleMessage(msg);
+											}
+
+										};
 
 }
